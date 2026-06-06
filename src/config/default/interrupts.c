f718bcd08fa8743250ae5b4a2bc219a438f4c6c3
saikit0123 <lamsaikitlam1994@hotmail.com>
2026-06-06 23:08:37 +0800

    can run

--- src/config/default/interrupts.c
+++ src/config/default/interrupts.c
@@ -76,7 +76,7 @@ void __attribute__((optimize("-O1"), long_call, noreturn, used))Dummy_Handler(vo
 }
 
 /* MISRAC 2012 deviation block start */
-/* MISRA C-2012 Rule 8.6 deviated 25 times.  Deviation record ID -  H3_MISRAC_2012_R_8_6_DR_1 */
+/* MISRA C-2012 Rule 8.6 deviated 22 times.  Deviation record ID -  H3_MISRAC_2012_R_8_6_DR_1 */
 /* Device vectors list dummy definition*/
 extern void PM_Handler                 ( void ) __attribute__((weak, noreturn, alias("Dummy_Handler")));
 extern void SYSCTRL_Handler            ( void ) __attribute__((weak, noreturn, alias("Dummy_Handler")));
@@ -104,7 +104,6 @@ extern void DAC_Handler                ( void ) __attribute__((weak, noreturn, a
 extern void PTC_Handler                ( void ) __attribute__((weak, noreturn, alias("Dummy_Handler")));
 extern void I2S_Handler                ( void ) __attribute__((weak, noreturn, alias("Dummy_Handler")));
 
-
 /* MISRAC 2012 deviation block end */
 
 /* Multiple handlers for vector */
@@ -127,12 +126,12 @@ const H3DeviceVectors exception_table=
     .pfnSYSCTRL_Handler            = SYSCTRL_Handler,
     .pfnWDT_Handler                = WDT_Handler,
     .pfnRTC_Handler                = RTC_Handler,
-    .pfnEIC_Handler                = EIC_Handler,
+    .pfnEIC_Handler                = EIC_InterruptHandler,
     .pfnNVMCTRL_Handler            = NVMCTRL_Handler,
     .pfnDMAC_Handler               = DMAC_Handler,
     .pfnUSB_Handler                = USB_Handler,
     .pfnEVSYS_Handler              = EVSYS_Handler,
-    .pfnSERCOM0_Handler            = SERCOM0_Handler,
+    .pfnSERCOM0_Handler            = SERCOM0_SPI_InterruptHandler,
     .pfnSERCOM1_Handler            = SERCOM1_Handler,
     .pfnSERCOM2_Handler            = SERCOM2_Handler,
     .pfnSERCOM3_Handler            = SERCOM3_Handler,
@@ -141,7 +140,7 @@ const H3DeviceVectors exception_table=
     .pfnTCC0_Handler               = TCC0_Handler,
     .pfnTCC1_Handler               = TCC1_Handler,
     .pfnTCC2_Handler               = TCC2_Handler,
-    .pfnTC3_Handler                = TC3_Handler,
+    .pfnTC3_Handler                = TC3_TimerInterruptHandler,
     .pfnTC4_Handler                = TC4_Handler,
     .pfnTC5_Handler                = TC5_Handler,
     .pfnADC_Handler                = ADC_Handler,
